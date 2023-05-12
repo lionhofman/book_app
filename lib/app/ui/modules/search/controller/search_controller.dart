048ba0a91108f0ba374c12de25d2edb53df0f878
Lion Hofman <lionhofman@gmail.com>
2023-05-12 19:15:16 -0300

    🔨refactor: update search page

--- lib/app/ui/modules/search/controller/search_controller.dart
+++ lib/app/ui/modules/search/controller/search_controller.dart
@@ -1,7 +1,11 @@
+import 'dart:ffi';
+
 import 'package:book_app/app/domain/entities/book.dart';
 import 'package:book_app/app/domain/entities/book_item.dart';
 import 'package:book_app/app/domain/usecases/search/get_autocomplete_list_use_case.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:get/get.dart';
 
 class SearchController extends GetxController {
@@ -18,14 +22,9 @@ class SearchController extends GetxController {
     this._getAutocompleteListUseCase,
   );
 
-  setText(String value) async {
-    _searchText.value = value;
-    if (_searchText.value.length > 3) {
-      getAutocompleteList(_searchText.value);
-    }
-  }
-
-  Future<void> getAutocompleteList(String text) async {
+  Future<List<BookItem>> getAutocompleteList(String text) async {
+    _searchText.value = text;
+    await Future.delayed(const Duration(seconds: 1));
     final result = await _getAutocompleteListUseCase.call(keyword: text);
     result.fold(
       (l) => null,
@@ -34,5 +33,26 @@ class SearchController extends GetxController {
         _autocompleteList.value = _book.value.items!;
       },
     );
+
+    return _autocompleteList;
+  }
+
+  void validateLaunchURL({required String? url}) async {
+    if (url != null) {
+      Uri myUri = Uri.parse(url);
+      await launchUrl(
+        myUri,
+        mode: LaunchMode.externalApplication,
+      );
+    }
+  }
+
+  String formatPrice({required String? price}) {
+    String priceString = "";
+    if (price != null) {
+      final formatCurrency = NumberFormat.simpleCurrency(locale: 'pt_BR');
+      priceString = formatCurrency.format(double.parse(price));
+    }
+    return priceString;
   }
 }
--- lib/app/ui/modules/search/search_page.dart
+++ lib/app/ui/modules/search/search_page.dart
@@ -13,35 +13,68 @@ class SearchPage extends GetView<SearchController> {
       padding: const EdgeInsets.all(8.0),
       child: TypeAheadField(
         textFieldConfiguration: TextFieldConfiguration(
+          autofocus: true,
+          style: DefaultTextStyle.of(context)
+              .style
+              .copyWith(fontStyle: FontStyle.italic),
           controller: controller.typeAheadController,
           decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            hintText: 'Search',
-          ),
+              prefixIcon: Icon(Icons.search),
+              hintText: 'Search',
+              border: OutlineInputBorder()),
         ),
         suggestionsCallback: (pattern) async {
-          await controller.setText(pattern);
-          return controller.autocompleteList;
-        },
-        itemBuilder: (context, suggestion) {
-          //     return ListView.builder(
-          //       physics: const BouncingScrollPhysics(),
-          // itemCount: controller.autocompleteList.length,
-          // shrinkWrap: true,
-          //       itemBuilder: (context, i){
-          //       return  Column(children: [
-
-          //       ],);
-          //     });
-
-          return ListTile(
-            leading: const Icon(Icons.shopping_cart),
-            title: Text(suggestion.volumeInfo!.title!),
-            subtitle: Text(suggestion.etag!),
-          );
+          if (pattern.isEmpty) {
+            return [];
+          }
+          return await controller.getAutocompleteList(pattern);
         },
         onSuggestionSelected: (suggestion) {
+          // controller.typeAheadController.text = suggestion.volumeInfo!.title!;
           Get.toNamed('');
+        },
+        suggestionsBoxDecoration: SuggestionsBoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          elevation: 8.0,
+          color: Theme.of(context).cardColor,
+        ),
+        itemBuilder: (context, suggestion) {
+          return ListTile(
+              leading: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 4,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: suggestion.volumeInfo!.imageLinks != null
+                    ? Image.network(
+                        suggestion.volumeInfo!.imageLinks!.smallThumbnail!,
+                        fit: BoxFit.cover)
+                    : Container(),
+              ),
+              title: Text(suggestion.volumeInfo!.title!),
+              subtitle: Text(suggestion.saleInfo != null &&
+                      suggestion.saleInfo!.retailPrice != null
+                  ? controller.formatPrice(
+                      price: suggestion.saleInfo!.retailPrice!.amount!)
+                  : ""),
+              trailing: Visibility(
+                visible: suggestion.saleInfo != null &&
+                    suggestion.saleInfo!.buyLink != null,
+                child: ElevatedButton(
+                  onPressed: () => controller.validateLaunchURL(
+                      url: suggestion.saleInfo!.buyLink),
+                  child: const Text('Comprar'),
+                ),
+              ));
         },
       ),
     );
